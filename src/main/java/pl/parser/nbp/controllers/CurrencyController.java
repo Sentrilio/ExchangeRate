bf27af8224a910fb6e446bdaf9040b9e89de282e
Sentrilio <dominik.kowalski@protonmail.com>
2019-05-11 15:30:17 +0200

    getting date directly from xml but it takes long time to retrive it

--- src/main/java/pl/parser/nbp/controllers/CurrencyController.java
+++ src/main/java/pl/parser/nbp/controllers/CurrencyController.java
@@ -1,8 +1,5 @@
 package pl.parser.nbp.controllers;
 
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpRequest;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,22 +7,28 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.client.RestTemplate;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
 import pl.parser.nbp.model.Exchange;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
 import java.net.URL;
-import java.text.ParseException;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import javax.servlet.http.HttpServletRequest;
+import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.StringReader;
 
 import org.w3c.dom.Document;
-
-import static com.sun.javafx.fxml.expression.Expression.equalTo;
+import org.w3c.dom.NodeList;
+import org.w3c.dom.Node;
+import org.w3c.dom.Element;
+import org.w3c.dom.Document;
+import org.xml.sax.InputSource;
 
 @Controller
 public class CurrencyController {
@@ -69,20 +72,33 @@ public class CurrencyController {
 				while ((inputLine = in.readLine()) != null) {
 					if (inputLine.startsWith("c")) {
 						formatedDateFromTxt = Integer.parseInt(inputLine.substring(inputLine.length() - 6));
-						if (formatedStartDate <= formatedDateFromTxt && formatedDateFromTxt <= formatedEndDate) {
+//						if (formatedStartDate <= formatedDateFromTxt && formatedDateFromTxt <= formatedEndDate) {
 //							System.out.println(formatedDateFromTxt);
-							System.out.println(inputLine);
+//							System.out.println(inputLine);
 
-							RestTemplate restTemplate = new RestTemplate();
-							String fooResourceUrl = "http://www.nbp.pl/kursy/xml/" + inputLine + ".xml";
-							ResponseEntity<String> response
-									= restTemplate.getForEntity(fooResourceUrl, String.class);
-							System.out.println(response.getBody());
+						RestTemplate restTemplate = new RestTemplate();
+						String resourceUrl = "http://www.nbp.pl/kursy/xml/" + inputLine + ".xml";
+						ResponseEntity<String> response
+								= restTemplate.getForEntity(resourceUrl, String.class);
+//						System.out.println(response.getBody());
+//						Document doc = convertStringToXMLDocument(response.getBody());
+
+						DocumentBuilder builder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+						InputSource src = new InputSource();
+						src.setCharacterStream(new StringReader(response.getBody()));
+						Document doc = builder.parse(src);
+						String data_publikacji = doc.getElementsByTagName("data_publikacji").item(0).getTextContent();
+						String kurs_kupna = doc.getElementsByTagName("kurs_kupna").item(0).getTextContent();
+						LocalDate tempDate = LocalDate.parse(data_publikacji, formatter);
+						if ((start.isEqual(tempDate) || start.isBefore(tempDate)) && (end.isEqual(tempDate) || end.isAfter(tempDate))) {
+							System.out.println(data_publikacji);
+						}
+//						System.out.println(kurs_kupna);
 
 //							Document doc = loadTestDocument("http://www.nbp.pl/kursy/xml/"+inputLine+".xml");
 //							System.out.println(doc);
-							break;
-						}
+//						break;
+//						}
 //						System.out.println(formatedStartDate);
 //						System.out.println(formatedDateFromTxt);
 					}
@@ -98,10 +114,18 @@ public class CurrencyController {
 
 	}
 
-	private Document loadTestDocument(String url) throws Exception {
+	private Document convertStringToXMLDocument(String xmlString) throws ParserConfigurationException, IOException, SAXException {
+		//Parser that produces DOM object trees from XML content
 		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-		factory.setNamespaceAware(true);
-		return factory.newDocumentBuilder().parse(new URL(url).openStream());
+
+		//API to obtain DOM Document instance
+		DocumentBuilder builder = factory.newDocumentBuilder();
+		//Create DocumentBuilder with default configuration
+
+		//Parse the content to Document object
+		Document doc = builder.parse(new InputSource(new StringReader(xmlString)));
+		doc.getDocumentElement().normalize();
+		return doc;
 	}
 
 }
